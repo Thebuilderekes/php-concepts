91b10359b0301833798cbd1db691b5ed66c15edf
thebuilderekes <ekeopreberedugo1@gmail.com>
2024-02-15 21:24:06 +0100

    learning PHP

---
+++ Basics/note.md
@@ -0,0 +1,10 @@
+# Variables
+
+- variables are case sensitive
+- variables always begin with a $ sign
+- don't use numbers after the $ sign
+
+**only letters, underscore are allowed to be used after the $ sign
+
+//this is a comment in php
+#his is also a comment in php
---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+      $developer = "Ekeopre";
+      $is_A_millionaire  = "is a millionaire";
+      $book = "hitchhiker's guide to the galaxy"; //you could use an escape sequence \ before the inner apostrophe but this is the best way to go about it.
+      $same_book_in_quotes = '"hitchhiker\'s guide to the galaxy"'; 
+      echo $developer, " ", $is_A_millionaire;
+      echo ' ';
+      echo $book;
+      echo ' ';
+      echo $same_book_in_quotes;
+      echo ' ';
+      echo " i am reading $same_book_in_quotes";
+//you don't have to use the closing angle brackets if PHP is the only thing uou are writing on the page
+//this is a comment in PHP
+#This is also a comment in PHP
+/* this is a multiline comment
+in PHP
+*/
